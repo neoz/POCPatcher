d0ebef10df0dda42a82158c97afc5ec172c76603
computer.angel <computer.angel@gmail.com>
2024-04-13 09:52:36 +0700

    - Add arguments in patcher

--- MedicinPatcher/Program.cs
+++ MedicinPatcher/Program.cs
@@ -1,5 +1,6 @@
 ﻿using dnlib.DotNet;
 using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,70 @@ namespace MedicinPatcher
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            var asm = typeof(Program).Assembly;
-            
-            ModuleDefMD module = ModuleDefMD.Load("SampleConsoleNeedCrack.exe");
+        {
+            var targetFile = "";
+            bool isNative = false;
+            if (args.Length > 0)
+            {
+                // "-n" : native mode patching
+                // "-h" : print help
+                // else : print help
+                if (args[0] == "-n")
+                {
+                    isNative = true;
+                }
+                else if (args[0] == "-h")
+                {
+                    printHelp();
+                    return;
+                }
+                else
+                {
+                    if (args[0].StartsWith("-"))
+                    {
+                        // print  invalid argument
+                        Console.WriteLine("Invalid argument: " + args[0]);
+                        Console.WriteLine();
+                        printHelp();
+                        return;
+                    }
+                    else
+                    { 
+                        // get argument as target file and check if it exists, if not then print error and return
+                        if (!System.IO.File.Exists(args[0]))
+                        {
+                            Console.WriteLine("File not found: " + args[0]);
+                            Console.WriteLine();
+                            printHelp();
+                            return;
+                        }
+                        targetFile = args[0];
+                    }
+                }
+            } else
+            {
+                printHelp();
+                return;
+            }
 
-            //var type1 = asm.GetType("MedicinPatcher.Medicin");
+            var asm = typeof(Program).Assembly;
+
+            /*
+             * try to generate output file name based on target file name
+             * if target file is SampleConsoleNeedCrack.exe then output file will be SampleConsoleNeedCrack-patched.exe
+             * if isNative is true then output file will be SampleConsoleNeedCrack-patched-native.exe
+            */
+            var outputFileName = targetFile.Replace(".exe", "-patched");
+            if (isNative)
+            {
+                outputFileName += "-native";
+            }
+            outputFileName += ".exe";
+
+            // Load target file
+            Console.WriteLine($"Loading target file {targetFile}");
+            ModuleDefMD module = ModuleDefMD.Load(targetFile);
+
             var type1 = typeof(mypcode.Medicin);
             Importer importer = new Importer(module);
             ITypeDefOrRef consoleRef = importer.Import(type1);
@@ -27,7 +86,29 @@ namespace MedicinPatcher
             var init = module.GlobalType.FindOrCreateStaticConstructor();
             init.Body.Instructions.Insert(0, Instruction.Create(OpCodes.Call, patchMethod));
 
-            module.Write("SampleConsoleNeedCrack-patched.exe");
+            if (isNative)
+            {
+                NativeModuleWriterOptions options = new NativeModuleWriterOptions(module,true);
+                options.MetadataOptions.Flags |= MetadataFlags.PreserveAll;
+                Console.WriteLine($"Writing native mode patched file {outputFileName}");
+                module.NativeWrite(outputFileName,options);
+            }
+            else
+            {
+                ModuleWriterOptions options = new ModuleWriterOptions(module);
+                options.MetadataOptions.Flags |= MetadataFlags.PreserveAll;
+                Console.WriteLine($"Writing patched file {outputFileName}");
+                module.Write(outputFileName, options);
+            }
+        }
+
+        private static void printHelp()
+        {
+            Console.WriteLine("Usage: MedicinPatcher.exe [option] targetfile");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            Console.WriteLine("  -n : Native mode patching");
+            Console.WriteLine("  -h : Print this help");
         }
     }
 }
